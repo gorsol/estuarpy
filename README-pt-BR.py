e075e0e0ef58ba2d94322b066c4ecf6b93499283
gsolana <gorsol@gmail.com>
2019-11-12 18:30:15 +0200

    Initial commit. Add readme pt

---
+++ README-pt-BR.py
@@ -0,0 +1,7 @@
+# estuarpy
+| [ENGLISH](./README.md)
+
+[<img src="./images/elsewhen-logo.png" width="180" height="180">](http://elsewhen.co/)
+
+# Padrões de Projeto &middot; [![PRs são bem vindos](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
+
