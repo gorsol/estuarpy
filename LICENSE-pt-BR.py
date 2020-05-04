f1dd0a722b26f392cf100462ae659429bf2d369c
gsolana <gorsol@gmail.com>
2020-05-04 23:22:46 +0200

    Update readme file

---
+++ LICENSE-pt-BR.py
@@ -0,0 +1,21 @@
+MIT License
+
+Copyright (c) 2020 [Gorka Solana](https://orcid.org/0000-0002-1212-0574)
+
+A permissão é concedida, gratuitamente, a qualquer pessoa que obtenha uma cópia
+deste software e dos arquivos de documentação associados (o "Software"), para 
+tarar o Software sem restrições, incluindo, sem limitação, os direitos de 
+uso, cópia, modificação, fusão , publicar, distribuir, sublicenciar e / ou vender 
+cópias do Software e permitir que as pessoas a quem o Software é fornecido o façam, 
+sob as seguintes condições:
+
+O aviso de direitos autorais acima e este aviso de permissão devem ser incluídos 
+em todas as cópias ou partes substanciais do Software.
+
+O SOFTWARE É FORNECIDO "TAL COMO ESTÁ", SEM GARANTIA DE QUALQUER TIPO, EXPRESSA
+OU IMPLÍCITA, INCLUINDO MAS NÃO SE LIMITANDO A GARANTIAS DE COMERCIALIZAÇÃO, 
+ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA E NÃO INFRACÇÃO. EM NENHUM CASO A AUTORES 
+OU TITULARES DE DIREITOS AUTORAIS SÃO RESPONSÁVEIS POR QUALQUER RECLAMAÇÃO, DANOS
+OU OUTRAS RESPONSABILIDADES, SEJA EM AÇÃO DE CONTRATO, TORT OU OUTRA FORMA, 
+DECORRENTE DE, FORA DE OU EM CONEXÃO COM O SOFTWARE OU O USO OU OUTROS NEGÓCIOS 
+NO SOFTWARE.
